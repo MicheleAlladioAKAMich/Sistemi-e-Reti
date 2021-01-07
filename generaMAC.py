d9c2b886f63095db06878bbc43ea4f38f50a6de5
Michele Alladio <61046970+MicheleAlladioAKAMich@users.noreply.github.com>
2021-01-07 11:37:50 +0100

    Update generaMAC.py

--- generaMAC.py
+++ generaMAC.py
@@ -2,22 +2,21 @@
 Author: Michele Alladio
 es: generare un indirizzo MAC
 """
-def main():
+import random, string   #importo le librerie
 
-    def generaMac():
-        import random, string   #importo le librerie
-
-        #si definisce il formato del messaggio -> 2 caratteri:2 caratteri:2 caratteri:2 caratteri:........
-        #successivamente si estraggono due caratteri random tra numeri e lettere maiuscole per 6 volte
-        return "%02s:%02s:%02s:%02s:%02s:%02s" % (  
-        random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
-        random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
-        random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
-        random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
-        random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
-        random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
-        )
+def generaMac():
+    #si definisce il formato del messaggio -> 2 caratteri:2 caratteri:2 caratteri:2 caratteri:........
+    #successivamente si estraggono due caratteri random tra numeri e lettere maiuscole per 6 volte
+    return "%02s:%02s:%02s:%02s:%02s:%02s" % (  
+    random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
+    random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
+    random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
+    random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
+    random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
+    random.choice(string.ascii_uppercase + string.digits) + random.choice(string.ascii_uppercase + string.digits),
+    )
     
+def main():
     mac = generaMac()   #richiamo la funzione per generare l'indirizzo MAC
     print(mac)  #stampo l'indirizzo MAC
 
