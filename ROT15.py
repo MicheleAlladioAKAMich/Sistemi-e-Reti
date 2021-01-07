df43b6679eea209795db7833de731f41ea9c7634
Michele Alladio <61046970+MicheleAlladioAKAMich@users.noreply.github.com>
2021-01-07 11:39:56 +0100

    Update ROT15.py

--- ROT15.py
+++ ROT15.py
@@ -9,32 +9,32 @@ listaROT15 = 'PQRSTUVWXYZABCDEFGHIJKLMNOpqrstuvwxyzabcdefghijklmnop'    #alfabet
 conversioneNormaleToROT15 = dict(zip(listaNormale, listaROT15)) #converte dall'alfabeto all'alfabeto ROT-15
 conversioneROT15ToNormale = dict(zip(listaROT15, listaNormale)) #converte dall'alfabeto ROT-15 all'alfabeto 
 
+def normaleToROT15(stringaEntrata): #conversione da alfabeto normale a ROT-15
+    listaAppoggio = []  
+    stringOutput = ''
+    for lettera in stringaEntrata:  #fino a quando non si finisce la stringa
+        if lettera in listaNormale: #se la lettera letta è presente nella lista dell'alfabeto
+            listaAppoggio.append(conversioneNormaleToROT15[lettera])    #viene effettuata la conversione e la lettera convertita viene posta al fondo della lista di appoggio
+        else:
+            listaAppoggio.append(lettera)   #altrimenti non si effettua nessuna conversione es: numeri
+
+    stringOutput = stringOutput.join(listaAppoggio) #il contenuto della lista di appoggio viene copiato nella stringa di output
+    return stringOutput
+
+def ROT15ToNormale(stringaEntrata): #conversione da ROT-15 ad alfabeto normale
+    listaAppoggio = []
+    stringOutput = ''
+    for lettera in stringaEntrata:  #fino a quando non si finisce la stringa
+        if lettera in listaNormale: #se la lettera letta è presente nella lista dell'alfabeto ROT-15
+            listaAppoggio.append(conversioneROT15ToNormale[lettera])    #viene effettuata la conversione e la lettera convertita viene posta al fondo della lista di appoggio
+        else:
+            listaAppoggio.append(lettera)      #altrimenti non si effettua nessuna conversione es: numeri
+
+    stringOutput = stringOutput.join(listaAppoggio) #il contenuto della lista di appoggio viene copiato nella stringa di output
+    return stringOutput
+
 def main():
-    def normaleToROT15(stringaEntrata): #conversione da alfabeto normale a ROT-15
-        listaAppoggio = []  
-        stringOutput = ''
-        for lettera in stringaEntrata:  #fino a quando non si finisce la stringa
-            if lettera in listaNormale: #se la lettera letta è presente nella lista dell'alfabeto
-                listaAppoggio.append(conversioneNormaleToROT15[lettera])    #viene effettuata la conversione e la lettera convertita viene posta al fondo della lista di appoggio
-            else:
-                listaAppoggio.append(lettera)   #altrimenti non si effettua nessuna conversione es: numeri
-    
-        stringOutput = stringOutput.join(listaAppoggio) #il contenuto della lista di appoggio viene copiato nella stringa di output
-        return stringOutput
-
-    def ROT15ToNormale(stringaEntrata): #conversione da ROT-15 ad alfabeto normale
-        listaAppoggio = []
-        stringOutput = ''
-        for lettera in stringaEntrata:  #fino a quando non si finisce la stringa
-            if lettera in listaNormale: #se la lettera letta è presente nella lista dell'alfabeto ROT-15
-                listaAppoggio.append(conversioneROT15ToNormale[lettera])    #viene effettuata la conversione e la lettera convertita viene posta al fondo della lista di appoggio
-            else:
-                listaAppoggio.append(lettera)      #altrimenti non si effettua nessuna conversione es: numeri
-    
-        stringOutput = stringOutput.join(listaAppoggio) #il contenuto della lista di appoggio viene copiato nella stringa di output
-        return stringOutput
-
-    stringa = input("Inserisci una stringa: ")
+    stringa = input("Inserisci una stringa (in alfabeto normale): ")
     print("ROT-15: " + normaleToROT15(stringa))
     print("ALFABETO NORMALE: " + ROT15ToNormale(normaleToROT15(stringa)))
 
