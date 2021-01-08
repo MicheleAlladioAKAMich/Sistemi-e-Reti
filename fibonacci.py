85f2ccc32ec20969cffe3d5cc3a1d7799019ec15
Michele Alladio <61046970+MicheleAlladioAKAMich@users.noreply.github.com>
2021-01-08 17:29:56 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,34 +2,29 @@
 Author: Michele Alladio
 es: serie di fibonacci entro una soglia impostata dall'utente
 """
+def fibonacciRicorsiva(num, k, primoN, secondoN, som): 
+    if num <= 0: print("non serve a molto :)")     #caso -> soglia = 0  
+    else:
+        if num == 1: print(primoN) #caso -> soglia = 1
+        else:    #caso -> soglia > 1
+            if k < 2:   #la prima volta che entra
+                print(primoN)   #printa il primo numero -> 1
+                print(secondoN)   #printa il secondo numero -> 1
+                k = 2   #forza il contatore a 2
+                fibonacciRicorsiva(num, k, primoN, secondoN, som)   #richiama la funzione ricorsiva
+            else:
+                if k < num: #fino a quando il contatore è minore della soglia
+                    k += 1  #aggiorna il valore del contatore
+
+                    som = primoN + secondoN  #calcola la somma
+                    print(som)  #stampa la somma
+
+                    primoN = secondoN  #aggiorna il valore del primo numero
+                    secondoN = som  #aggiorna il valore del secondo numero
+
+                    fibonacciRicorsiva(num, k, primoN, secondoN, som)   #richiama la funzione ricorsiva
 
 def main():
-
-    def fibonacciRicorsiva(num, k, primoN, secondoN, som): 
-        if num == 0:    #caso -> soglia = 0
-            print("non serve a molto :)")   
-        else:
-            if num == 1:    #caso -> soglia = 1
-                print(primoN)
-            else:    #caso -> soglia > 1
-                if k < 2:   #la prima volta che entra
-                    print(primoN)   #printa il primo numero -> 1
-                    print(secondoN)   #printa il secondo numero -> 1
-                    k = 2   #forza il contatore a 2
-                    fibonacciRicorsiva(num, k, primoN, secondoN, som)   #richiama la funzione ricorsiva
-                else:
-                    if k < num: #fino a quando il contatore è minore della soglia
-                        k += 1  #aggiorna il valore del contatore
-
-                        som = primoN + secondoN  #calcola la somma
-                        print(som)  #stampa la somma
-
-                        primoN = secondoN  #aggiorna il valore del primo numero
-                        secondoN = som  #aggiorna il valore del secondo numero
-
-                        fibonacciRicorsiva(num, k, primoN, secondoN, som)   #richiama la funzione ricorsiva
-    
-
     #inizializzo le varibili che mi servono
     cnt = 0
     primoNumero = 1 
